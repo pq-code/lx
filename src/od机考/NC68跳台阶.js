3e39b5451473aafc8a84f0bbb63025e29e032309
p <784312297@qq.com>
2023-08-07 02:25:42 +0800

    存档

--- src/od机考/NC68跳台阶.js
+++ src/od机考/NC68跳台阶.js
@@ -1,11 +1,19 @@
 function jumpFloor (number) {
-    // write code here
-    // const results = [];
-    console.log(number)
+    // 递归
     if (number == 1 || number == 0) return 1;
     if (number == 2) return 2;
     return jumpFloor(number - 1) + jumpFloor(number - 2);
+    // 循环
+    // if (number < 2) {
+    //     return number
+    // }
+    // let last1 = 2; let last2 = 1
+    // for (let i = 3; i <= number; i++) {
+    //     last1 = last1 + last2;
+    //     last2 = last1 - last2;
+    // }
+    // return last1
 }
 
 
-console.log(jumpFloor(4))
+console.log(jumpFloor(7))
